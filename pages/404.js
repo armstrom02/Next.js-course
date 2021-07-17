3012d960a76bafa4815c412f2b3e575feb85ee52
armstrom02 <viveksinghreborn@gmail.com>
2021-07-17 16:26:24 +0530

    custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+function NotFoundPage() {
+    return (
+        <div>
+            <h1>Page Not Found</h1>
+        </div>
+    )
+}
+
+export default NotFoundPage
