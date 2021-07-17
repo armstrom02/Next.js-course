937fa6dee9cfbce4e4a9001f6862b14379a543c1
armstrom02 <viveksinghreborn@gmail.com>
2021-07-17 16:22:46 +0530

    Links and programatically route

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,14 +1,24 @@
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router'
 
 function ClientProjectPage() {
     const router = useRouter();
 
-    console.log(router.pathname);
     console.log(router.query);
+
+    function loadProjecctHandler() {
+        // load data...
+        // router.push('/clients/vivek/projecta');
+        router.push({
+            pathname: '/clients/[id]/[clientprojectid]',
+            query: { id: 'vivek', clientprojectid: 'projecta' }
+        });
+
+    }
 
     return (
         <div>
             <h1>The Client Project Page</h1>
+            <button onClick={loadProjecctHandler}>Load Project A</button>
         </div>
     )
 }
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,11 +1,24 @@
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 function clientsPage() {
-    const router = useRouter();
+
+    const clients = [
+        { id: 'vivek', name: 'vivek' },
+        { id: 'singh', name: 'singh' },
+    ]
 
     return (
         <div>
             <h1>The clientsPage Page</h1>
+            <ul>
+                {clients.map(client => <li key={client.id}><Link
+                    href={{
+                        pathname: '/clients/[id]',
+                        query: { id: client.id }
+                    }}
+                >{client.name}</Link></li>)}
+            </ul>
         </div>
     )
 }
